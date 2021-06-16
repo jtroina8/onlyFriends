9e3d5f64e8f6ab3c29fa99523096aa2f6fb8313a
Tatien Miller <tatmil99@gmail.com>
2021-06-16 14:58:48 -0500

    sets up server to interact with react front end

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// CREATES A USER
+router.post('/users', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const newUser = await User.create({
+    firstName, 
+    lastName, 
+    email, 
+    password,
+  });
+
+  res.json({
+    id: newUser.id
+  });
+});
+
+// DELETES A USER
+router.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedUser = await User.destroy({
+      where: {
+          id
+      }
+  });
+  res.json(deletedUser);
+});
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
 module.exports = router;
