0d93c3cc3ffd149e69a76e3083efeaaef681f1de
Joey <joey@Joeys-Mac-mini.lan>
2021-06-17 00:29:52 -0400

    did the css for the login page, and edited some signup input types

--- queue-app/src/components/LogIn.js
+++ queue-app/src/components/LogIn.js
@@ -1,13 +1,17 @@
 import React from "react";
+import OnlyFriends from "./assets/images/OnlyFriends.png";
 
 export default function LogIn() {
   return (
     <div className="log-in">
       <div className="log-in__container">
-        <h3>Log In to OnlyFriends</h3>
-        <input type="text" placeholder="Username, Phone, or Email" />
-        <input type="text" placeholder="Password" />
-        <button>Log In</button>
+        <img src={OnlyFriends} alt="" />
+        <h1>Log In to OnlyFriends</h1>
+        <div className="log-in__input-container">
+          <input type="text" placeholder="Username, Phone, or Email" />
+          <input type="password" placeholder="Password" />
+          <button className="log-in__btn">Log In</button>
+        </div>
       </div>
     </div>
   );
--- queue-app/src/components/SignUp.js
+++ queue-app/src/components/SignUp.js
@@ -6,10 +6,10 @@ export default function SignUp() {
       <div className="sign-up__container"></div>
       <h3>Sign Up for OnlyFriends</h3>
       <input type="text" placeholder="Username" />
-      <input type="text" placeholder="password" />
+      <input type="password" placeholder="password" />
       <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Email" />
-      <input type="text" placeholder="Phone" />
+      <input type="email" placeholder="Email" />
+      <input type="tel" placeholder="Phone" />
       <button>Sign Up</button>
     </div>
   );
