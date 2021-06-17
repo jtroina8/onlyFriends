4080c51811fdc37ab1215f5c0b36738e482ef2aa
Joey <80225246+jtroina8@users.noreply.github.com>
2021-06-17 13:11:00 -0400

    Revert "re-publishing Tates branch"

--- queue-app/src/App.js
+++ queue-app/src/App.js
@@ -1,11 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useState } from "react";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
