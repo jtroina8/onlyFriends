0f525a0b35bab8238686bd6ae1e7a6605e8711cc
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-06-21 11:39:15 -0500

    changed config to match master

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-module.exports = 
-{
-  "development": {
-    "username": process.env.USERNAME, 
-    "password": process.env.PASSWORD || null,
-    "database": "onlyfriends",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD || null,
+    database: "onlyfriends",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
+
