113c08da8ce988f323d3e38d8765e2d0815fe0e3
Joey <joey@Joeys-Mac-mini.lan>
2021-06-21 11:02:00 -0400

    fixed the pics in gamebar

--- queue-app/src/components/GamingBar.js
+++ queue-app/src/components/GamingBar.js
@@ -1,59 +1,29 @@
 import React from "react";
+import val_boxart from "./assets/images/games/val_boxart.jpeg";
+import lol_boxart from "./assets/images/games/lol_boxart.jpeg";
+import apex_boxart from "./assets/images/games/apex_boxart.jpeg";
+import csgo_boxart from "./assets/images/games/csgo_boxart.jpeg";
+import ow_boxart from "./assets/images/games/ow_boxart.jpeg";
+import warzone_boxart from "./assets/images/games/warzone_boxart.jpeg";
+import fifa_boxart from "./assets/images/games/fifa_boxart.jpeg";
+import ssbu_boxart from "./assets/images/games/ssbu_boxart.jpeg";
+import nba2k_boxart from "./assets/images/games/nba2k_boxart.jpeg";
+import des2_boxart from "./assets/images/games/des2_boxart.jpeg";
 
 export default function GamingBar({ setGame }) {
   return (
     <div>
       <div className="game-bar">
-        <img
-          className="val_boxart"
-          src="https://cdn-ascope-prod.global.ssl.fastly.net/static/images/boxart_valorant_300x400.jpg"
-          alt=""
-        />
-        <img
-          className="lol_boxart"
-          src="https://cdn-ascope-prod.global.ssl.fastly.net/static/images/boxart_league_of_legends_300x400.jpg"
-          alt=""
-        />
-        <img
-          className="apex_boxart"
-          src="https://www.mobygames.com/images/covers/l/538006-apex-legends-xbox-one-front-cover.jpg"
-          alt=""
-        />
-        <img
-          className="csgo_boxart"
-          src="https://www.gamespot.com/a/uploads/scale_medium/mig/7/2/0/5/2227205-i2cs9uzmq4yua.jpg"
-          alt=""
-        />
-        <img
-          className="ow_boxart"
-          src="https://upload.wikimedia.org/wikipedia/en/5/51/Overwatch_cover_art.jpg"
-          alt=""
-        />
-        <img
-          className="warzone_boxart"
-          src="https://howlongtobeat.com/games/75852_Call_of_Duty_Warzone.jpg"
-          alt=""
-        />
-        <img
-          className="fifa_boxart"
-          src="https://sm.ign.com/ign_in/screenshot/default/ediffc5xkacogex_czd1.jpg"
-          alt=""
-        />
-        <img
-          className="nba2k_boxart"
-          src="https://hoopshype.com/wp-content/uploads/sites/92/2020/07/damian-lillard-2k21-a.jpg?w=794"
-          alt=""
-        />
-        <img
-          className="ssbu_boxart"
-          src="https://wbpl.org/wp-content/uploads/2018/12/Super-Smash-Bros-Ultimate-Box-Art.jpg"
-          alt=""
-        />
-        <img
-          className="des2_boxart"
-          src="https://cdn-ascope-prod.global.ssl.fastly.net/static/images/boxart_destiny_2_300x400.jpg"
-          alt=""
-        />
+        <img className="val_boxart" src={val_boxart} alt="" />
+        <img className="lol_boxart" src={lol_boxart} alt="" />
+        <img className="apex_boxart" src={apex_boxart} alt="" />
+        <img className="csgo_boxart" src={csgo_boxart} alt="" />
+        <img className="ow_boxart" src={ow_boxart} alt="" />
+        <img className="warzone_boxart" src={warzone_boxart} alt="" />
+        <img className="fifa_boxart" src={fifa_boxart} alt="" />
+        <img className="ssbu_boxart" src={ssbu_boxart} alt="" />
+        <img className="nba2k_boxart" src={nba2k_boxart} alt="" />
+        <img className="des2_boxart" src={des2_boxart} alt="" />
       </div>
       <div className="game-cards"></div>
     </div>
