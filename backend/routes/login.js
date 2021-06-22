cc9a35840b5b204d873322ce0b319ea3cec5bdd5
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-06-22 10:03:07 -0500

    bcrypt working

---
+++ backend/routes/login.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
+
+
+
+router.post("login", (req, res) => {
+
+})
+
+module.exports = router;
