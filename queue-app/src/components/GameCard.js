944cde33ac2c2742288d0cb7c3c5e1f8db41d79f
Joey <joey@Joeys-Mac-mini.local>
2021-07-14 13:44:43 -0400

    changed profile a bit

--- queue-app/src/components/GameCard.js
+++ queue-app/src/components/GameCard.js
@@ -56,6 +56,50 @@ export default function GameCard() {
           </div>
         </div>
       </div>
+
+      <div className="val-card">
+        <div className="val-card__info"></div>
+        <div className="val-card__user-info">
+          <h2>VALORANT:</h2>
+          <div className="val-card__user-rank">
+            <h4>Rank: </h4>
+            <div className="val-card__user-rank-symbol">
+              <img
+                src="https://media.valorant-api.com/competitivetiers/564d8e28-c226-3180-6285-e48a390db8b1/13/largeicon.png"
+                alt="gold-2"
+              />
+            </div>
+            <h4>Peak: </h4>
+            <div className="val-card__user-peak-rank">
+              <img
+                src="https://media.valorant-api.com/competitivetiers/564d8e28-c226-3180-6285-e48a390db8b1/17/largeicon.png"
+                alt="plat-3"
+              />
+            </div>
+          </div>
+          <div className="val-card__user-characters">
+            <h4>Mains: </h4>
+            <img
+              src={gameData === [""] ? "" : gameData[0].displayIconSmall}
+              alt="breach-pic"
+            />
+            <img src={valPhoenix} alt="phoenix-pic" />
+            <img src={valReyna} alt="reyna-pic" />
+          </div>
+          <div className="val-card__user-words">
+            <div className="val-card__user-stats">
+              <div className="val-card__user-wr">
+                <h4>Win Rate: </h4>
+                <h1> 55.3%</h1>
+              </div>
+              <div className="val-card__user-weapon">
+                <h4>Weapon of Choice:</h4>
+                <h1>Vandal</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
